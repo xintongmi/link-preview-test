8487f496bea13d78de987513d315b5cf84358c0e
Cindy Mi <84825750+XintongTheCoder@users.noreply.github.com>
2025-01-02 23:26:28 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+function App() {
+  return (
+    <div>
+    <Helmet>
+      <meta property="og:title" content="Alex Hayes - CSU" />
+      <meta property="og:image" content="https://app.joinhandshake.com/user_profile_photos/53790821?size=profile" />
+      <meta property="og:url" content="https://app.joinhandshake.com/profiles/67xfn7" />
+      <meta property="og:type" content="website" />
+    </Helmet>
+    <h1>Welcome to My App?????</h1>
+  </div>
+  );
+}
+
+export default App;
